13079c7c63f20aa94b73a8cc4529fba11b89a1c3
Vlad Shepshuk <vgoilik@gmail.com>
2017-12-05 15:39:46 +0300

    Added a WatchOptions as well as Liver reload via webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
 
-var css = require("./output.css");
+//var css = require("./output.css");
 
-var newLink = document.createElement('link');
-newLink.setAttribute('href', css);
-newLink.setAttribute('rel', 'stylesheet');
-document.head.appendChild(newLink);
-
+//var newLink = document.createElement('link');
+//newLink.setAttribute('href', './output.css');
+//newLink.setAttribute('rel', 'stylesheet');
+//document.head.appendChild(newLink);
+//alert('hi, brother, my man!!!');
+alert("show me what u got,CJJJJJJJJJJ!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 'use strict';
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        main: ["webpack-dev-server/client", "./src/index.js"]
+    },
     output: {
         filename: "./bundle.js",
         path: path.resolve(__dirname, 'dist')
@@ -12,7 +15,26 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
+    },
+
+
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        ignored: /node_modules/
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        contentBase: __dirname + '/dist',
+        hot: true
     }
+
 };
 
 
