9be996e1bccec40956d994221618220f09160a2a
Vlad Shepshuk <vgoilik@gmail.com>
2017-12-09 23:11:17 +0300

    es2015

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 "use strict";
-//let path = require("path");
+
 //let css = require("../dist/CSS/app.css");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 let createLinkTagInDOM = () => {
     let newLink = document.createElement('link');
     newLink.setAttribute('href', '/CSS/app.css');
@@ -8,7 +12,22 @@ let createLinkTagInDOM = () => {
     document.head.appendChild(newLink);
 };
 
+let timeIn = () => {
+    const element = (
+        <div>
+            <h1>Текущее время в Минске:</h1>
+            <h2>Сейчас {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+};
+
 
 createLinkTagInDOM();
 
+setInterval(timeIn, 1000);
+
+
+
 //<link href="output.css" rel="stylesheet">
+
