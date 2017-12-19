ccc4f5291af2582632c544d0de9254fd8ace9702
Vlad Shepshuk <vgoilik@gmail.com>
2017-12-20 02:20:47 +0300

    added class components to render columns

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Leftcolumn extends React.Component {
+    render() {
+        return (
+            <li className="sidebar">
+                Левая колонка
+            </li>
+        );
+    }
+}
+const placeWeWantToPutComponent = document.getElementById('qwerty');
+
+class Middlecolumn extends React.Component {
+    render() {
+        return (
+            <li className="article">
+                Текст...
+            </li>
+        );
+    }
+}
+
+class Rightcolumn extends React.Component {
+    render() {
+        return (
+            <div className="flex-container">
+                <Leftcolumn />
+                <Middlecolumn />
+                <li className="aside">
+                    Правая колонка
+                </li>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<Rightcolumn />, placeWeWantToPutComponent);
+
+
 let createLinkTagInDOM = () => {
     const newLink = (
         <link href="/CSS/app.css" rel="stylesheet">
